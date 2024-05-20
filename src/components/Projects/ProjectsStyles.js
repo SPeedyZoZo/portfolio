e4a7f1442365e83f3d70b83aa8425c8368465499
SPeedyZoZo <zaidchu@gmail.com>
2024-05-21 02:41:14 +0300

    Added minimum height + fit images for project cards.  Added break line between project stack and project description

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const Img = styled.img`
-  width:100%;
-  height:100%;
+  width: 100%;
+  height: 200px; /* Fixed height for consistency */
   object-fit: cover;
   overflow: hidden;
-`
+`;
 
 export const GridContainer = styled.section`
 display: grid;
@@ -27,6 +27,7 @@ export const BlogCard = styled.div`
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   width: 400px;
+  min-height: 700px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }

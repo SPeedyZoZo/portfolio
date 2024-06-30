123299786da202c3c5fec6b1d716fa089f800981
SPeedyZoZo <zaidchu@gmail.com>
2024-06-30 19:08:34 +0300

    Added plerdy tracking code to _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,6 +31,15 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
+          <script type="text/javascript" defer data-plerdy_code='1'>
+              var _protocol="https:"==document.location.protocol?"https://":"http://";
+              _site_hash_code = "9ef0bb75422af803aaf41e71ada5c46c",_suid=52165, plerdyScript=document.createElement("script");
+              plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
+              plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
+              var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
+              plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
+              try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
+          </script>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>

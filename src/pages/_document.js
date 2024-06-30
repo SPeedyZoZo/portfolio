9a6066debfd91f47e52bf48125fc10985f3a5e5e
SPeedyZoZo <zaidchu@gmail.com>
2024-06-30 19:32:50 +0300

    altered _document.js to be compatible

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,19 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,25 +22,36 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
+
   render() {
     return (
       <Html lang='en-GB'>
         <Head>
-          <script type="text/javascript" defer data-plerdy_code='1'>
-              var _protocol="https:"==document.location.protocol?"https://":"http://";
-              _site_hash_code = "9ef0bb75422af803aaf41e71ada5c46c",_suid=52165, plerdyScript=document.createElement("script");
-              plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
-              plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
-              var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
-              plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
-              try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
-          </script>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          {/* Plerdy Tracking Code */}
+          <script
+            type="text/javascript"
+            defer
+            dangerouslySetInnerHTML={{
+              __html: `
+                var _protocol="https:"==document.location.protocol?"https://":"http://";
+                _site_hash_code = "9ef0bb75422af803aaf41e71ada5c46c",_suid=52165, plerdyScript=document.createElement("script");
+                plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
+                plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
+                var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
+                plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
+                try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
